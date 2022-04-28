ebdda89cc9e3135110b3d38fb5c48dab1790ce62
Dark-Bag <66421135+Dark-Bag@users.noreply.github.com>
2022-04-28 10:58:32 +0200

    Created the shopping cart library

---
+++ shoppingCartSln/shoppingCart/Program.cs
@@ -0,0 +1,14 @@
+﻿using ShoppingCartLib;
+using System;
+
+namespace shoppingCart
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 test = new Class1();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
